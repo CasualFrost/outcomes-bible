7cbc44a6e1d4495d3e8e8079ad8ada1ea85fd69e
CasualFrost <simplyjasonfrost@gmail.com>
2022-02-14 07:17:46 -0800

    styling part 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 export default function Footer() {
     return (
         <footer id="footer">
-            <h3 class="spaceten fontstyle right">Cocktail Planner</h3>
+            <h3 class="spaceten fontstyle right">Outcomes Bible</h3>
             <p class="fontstyle left">Terms of Service</p>
             <p class="fontstyle left">Contact Us</p>
         </footer>
